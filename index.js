7e54fc903fbaa4ac06a33a9e1dbc5de3afc8d45a
Sameony <iam2001sam@gmail.com>
2021-09-12 16:34:11 +0530

    feat: Converts date to str and checks palindrome

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 const date_input= document.querySelector("#date-input");
 const btn_check = document.querySelector("#btn-check");
 
+var ourDate={
+    day:"",
+    month:"",
+    year:""
+}
 btn_check.addEventListener("click",function clickEventHandler(){
-    console.log(typeof(date_input.value));
-    console.log("check")
+    toProperDate(date_input.value);
+    var curDate =  ourDate.day+ourDate.month+ourDate.year;
+    console.log(curDate);
+    isCurDatePalindrome(curDate);
 });
 
+function toProperDate(date){
+    ourDate.day = date.slice(-2);
+    ourDate.month = date.slice(5,7);
+    ourDate.year = date.slice(0,4);
+}
+
+function isCurDatePalindrome(date)
+{
+    var flag=true;
+    var length = date.length;
+    for(var i=0;i<Math.trunc(length/2);i++)
+    {
+        if(date[i]!=date[length-i-1])
+            flag=false;
+    }
+    return flag;
+}
